e35b39c39f2547235517baf3778d21f791a7c05f
achonor <lidaha.004@hotmail.com>
2018-07-12 00:47:01 +0800

    添加文字表

---
+++ Assets/Script/Common/Language.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Language {
+    private static Dictionary<int, string> textDict = new Dictionary<int, string>() {
+        //等级
+        {10001, @"青铜" },
+        {10002, @"白银" },
+        {10003, @"黄金" },
+        {10004, @"铂金" },
+        {10005, @"钻石" },
+        //熟练度
+        {10101, @"脚趾" },
+        {10102, @"小腿" },
+        {10103, @"膝盖" },
+        {10104, @"大腿" },
+        {10105, @"屁股" },
+        {10106, @"肚子" },
+        {10107, @"胸膛" },
+        {10108, @"脖子" },
+        {10119, @"大脑" }
+    };
+
+
+    public static string GetTextByKey(int key)
+    {
+        string ret = "";
+        if (textDict.TryGetValue(key, out ret))
+        {
+            return ret;
+        }
+        return ret;
+    }
+}
